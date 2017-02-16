3fca3cc474661dda7845bf846f9e6d9d65ebb8f6
Your Name <you@example.com>
2017-02-16 20:02:23 +0100

    Add calculator project Spring Boot skeleton

---
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.leszko.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+}
