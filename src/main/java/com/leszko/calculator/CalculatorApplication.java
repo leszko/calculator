f6828c45c7b4cd1a68cedf4cf1505bd77168f80d
leszko <rafal.leszko@gmail.com>
2017-02-20 09:19:07 +0100

    Check Jenkins trigger

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Main Spring Application
+ * Main Spring Application.
  */
 @SpringBootApplication
 public class CalculatorApplication {
