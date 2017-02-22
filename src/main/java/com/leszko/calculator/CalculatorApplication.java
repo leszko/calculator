ff31fff63acace87296426fb02dfe3fc8bd14c46
leszko <rafal.leszko@gmail.com>
2017-02-22 07:11:02 +0100

    Check auto build

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Main Spring Application.
+ * Main Spring Application. Test
  */
 @SpringBootApplication
 public class CalculatorApplication {
