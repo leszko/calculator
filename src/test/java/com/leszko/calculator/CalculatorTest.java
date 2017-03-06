9168e6138c901d708ff4833b8fc76dc9a8296342
leszko <rafal.leszko@gmail.com>
2017-03-06 08:36:15 +0100

    Add Javadoc for tests.

--- src/test/java/com/leszko/calculator/CalculatorTest.java
+++ src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -2,6 +2,9 @@ package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Calculator test.
+ */
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
