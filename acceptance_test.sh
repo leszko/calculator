5ef8e5cc43837ea4ac3ccf6c07f0c0b85fbe37bb
leszko <rafal.leszko@gmail.com>
2017-03-16 18:21:31 +0100

    Change acceptance testing to Docker Compose

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-CALCULATOR_PORT=2345
-echo $CALCULATOR_PORT
+CALCULATOR_PORT=`docker-compose port calculator 8080 | cut -d: -f2`
+test `curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2` -eq 3
