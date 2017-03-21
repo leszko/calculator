dbcb159cb4174affe30738a53e261291fcf3dd86
leszko <rafal.leszko@gmail.com>
2017-03-21 09:00:58 +0100

    Fix checkstyle

--- src/test/java/acceptance/AcceptanceTest.java
+++ src/test/java/acceptance/AcceptanceTest.java
@@ -4,6 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+/** Runner for the acceptance test. */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:feature")
 public class AcceptanceTest {
