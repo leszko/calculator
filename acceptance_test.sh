377f655d74f1f262127d3c13a9d52950d87bc3f5
Your Name <you@example.com>
2017-04-08 18:00:30 +0200

    Fix acceptance test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CALCULATOR_PORT=$(ssh ubuntu@$@ "docker-compose port calculator 8080 | cut -d: -f2")
-./gradlew acceptanceTest -Dcalculator.url=http://$@:@CALCULATOR_PORT
+./gradlew acceptanceTest -Dcalculator.url=http://$@:$CALCULATOR_PORT
