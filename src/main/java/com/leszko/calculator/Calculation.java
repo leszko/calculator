6ddc072000a49a1f3773dcde92157f20476b65c6
leszko <rafal.leszko@gmail.com>
2017-06-19 19:14:24 +0200

    Add "created_at" column to the "calculation" table.

--- src/main/java/com/leszko/calculator/Calculation.java
+++ src/main/java/com/leszko/calculator/Calculation.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.sql.Timestamp;
 
 @Entity
 public class Calculation {
@@ -13,12 +14,14 @@ public class Calculation {
     private String a;
     private String b;
     private String result;
+    private Timestamp createdAt;
 
     protected Calculation() {}
 
-    public Calculation(String a, String b, String result) {
+    public Calculation(String a, String b, String result, Timestamp createdAt) {
         this.a = a;
         this.b = b;
         this.result = result;
+        this.createdAt = createdAt;
     }
 }
--- src/main/java/com/leszko/calculator/CalculatorController.java
+++ src/main/java/com/leszko/calculator/CalculatorController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
 @RestController
 class CalculatorController {
     @Autowired
@@ -15,7 +18,8 @@ class CalculatorController {
     @RequestMapping("/sum")
     String sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
         String result = String.valueOf(calculator.sum(a, b));
-        calculationRepository.save(new Calculation(a.toString(), b.toString(), result));
+        Calculation calculation = new Calculation(a.toString(), b.toString(), result, Timestamp.from(Instant.now()));
+        calculationRepository.save(calculation);
         System.out.println("Count: " + calculationRepository.count());
         return result;
     }
