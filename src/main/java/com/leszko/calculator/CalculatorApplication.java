04331fd44ff525cb3e6a19d963573b3c2ea2a1f4
Rafal Leszko <rafal@livepeer.org>
2021-12-16 15:46:55 +0100

    Test constant

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
+	private static final String constant = "constant";
+
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
